e9e6a2055a642c493c0fa4b66eb1f679c20bb4ae
Abhishek SIngh <abhisheksingh.a97@gmail.com>
2020-06-25 00:39:38 +0530

    Adding Vector Files

---
+++ Assets/2.Vectors/Move2Goal.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move2Goal : MonoBehaviour
+{
+    public float speed = 2.0f;
+    public Transform goal;
+    public float accuracy = 0.01f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        this.transform.LookAt(goal.position);
+        Vector3 direction = goal.position - this.transform.position;
+        Debug.DrawRay(this.transform.position, direction,Color.red);
+        if (direction.magnitude > accuracy)
+        {
+            this.transform.Translate(direction.normalized * speed * Time.deltaTime,Space.World);
+        }
+    }
+}
